a19d73ef8d898825cf3c832c3e994de30787f339
Sven Prinsloo <sven@LAWTrust.co.za>
2018-07-30 17:24:26 +0200

    Mutual Authentication Testing
    Mutual Authentication Testing between client and server.

---
+++ TestMutualSSL/App_Code/ClientSSLSetup.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+
+/// <summary>
+/// Summary description for ClientSSLSetup
+/// </summary>
+public static class ClientSSLSetup
+{
+    public static X509Certificate2 CreateMutualSslClient(string serial)
+    {
+        X509Certificate2 clientCertificate;
+        if (TryFindClientCertificate(serial, out clientCertificate))
+        {
+            return clientCertificate;
+        }
+        return null;
+    }
+
+    public static bool TryFindClientCertificate(string clientCertSerial, out X509Certificate2 certificate)
+    {
+        certificate = null;
+        const StoreName personal = StoreName.My;
+        const StoreLocation currentUser = StoreLocation.CurrentUser;
+        const StoreLocation localMachine = StoreLocation.LocalMachine;
+        var store = new X509Store(personal, currentUser);
+        var store2 = new X509Store(personal, localMachine);
+        var clientCert = searchCertificateSerial(clientCertSerial, ref store);
+        if (clientCert != null)
+        {
+            certificate = clientCert;
+            return true;
+        }
+
+        clientCert = searchCertificateSerial(clientCertSerial, ref store2);
+
+        return clientCert != null;
+    }
+
+    public static X509Certificate2 searchCertificateSerial(string certificateSerial, ref X509Store store)
+    {
+        var certFound = false;
+        X509Certificate2 clientCertificate = null;
+        try
+        {
+            store.Open(OpenFlags.ReadOnly);
+            var certificates = store.Certificates;
+            foreach (var certificate in certificates)
+            {
+                if (certificate.SerialNumber != null &&
+                    certificate.SerialNumber.Equals(certificateSerial.ToUpper()))
+                {
+                    certFound = true;
+                    clientCertificate = certificate;
+                    store.Close();
+                    break;
+                }
+            }
+
+            if (!certFound)
+            {
+
+            }
+
+            // Log.Debug("Client Certificate not found in store.");
+        }
+        catch (Exception ex)
+        {
+            // Log.Debug("Failed to find client certificate. General exception occurred", ex);
+        }
+
+        return clientCertificate;
+    }
+}
---
+++ TestMutualSSL/App_Code/TestMutualSSL.cs
@@ -0,0 +1,53 @@
+﻿using System.Web.Services;
+using System.Net;
+using System;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+
+/// <summary>
+/// This Web Service is to test Mutual Authentication between the client and a server
+/// </summary>
+[WebService(Namespace = "TestMutualSSL/v1")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+public class TestMutualSSL : System.Web.Services.WebService
+{
+    public static WebProxy Proxy;
+    [WebMethod]
+    public string TestSsl(string certSerial, string requestString, string url)
+    {
+        var response = "";
+        try
+        {
+            //check input
+            if (string.IsNullOrEmpty(certSerial)) return response = "Cert Serial is null or empty";
+            if (string.IsNullOrEmpty(requestString)) return response = "Request String is null or empty";
+            if (string.IsNullOrEmpty(url)) return response = "URL is null or empty";
+
+            //get SSL certificate from certSerial
+            if (ClientSSLSetup.CreateMutualSslClient(certSerial) != null)
+            {
+                X509Certificate clientCert = ClientSSLSetup.CreateMutualSslClient(certSerial);
+                var req = (HttpWebRequest) WebRequest.Create(url);
+                req.ContentType = "text/xml";
+                req.Method = "GET";
+                req.UserAgent = "Identify Agent";
+                req.ClientCertificates.Add(clientCert);
+                req.AuthenticationLevel = AuthenticationLevel.MutualAuthRequired;
+                req.GetResponse();
+                response = "Successfully connected with Mutual Authentication";
+            }
+            else
+            {
+                response = "Error Occurred";
+            }
+        }
+        catch (Exception ex)
+        {
+            response = "Error Occurred " + ex.Message;
+        }
+        return response;
+        }
+}
+
+
